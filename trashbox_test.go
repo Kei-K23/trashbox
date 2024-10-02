d69f256c487f05d190f6d3c871a7153fa0b1a577
Kei-K23 <keiksl2301@gmail.com>
2024-10-02 21:11:18 +0630

    Add simple test

--- trashbox_test.go
+++ trashbox_test.go
@@ -1,19 +1,9 @@
 package trashbox
 
 import (
-	"os"
 	"testing"
 )
 
 func TestMoveToTrash(t *testing.T) {
-	testFile, err := os.CreateTemp("", "test_trashbox")
-	if err != nil {
-		t.Fatalf("unable to create temp file: %v", err)
-	}
-	defer os.Remove(testFile.Name())
 
-	err = MoveToTrash(testFile.Name())
-	if err != nil {
-		t.Fatalf("failed to move file to trash: %v", err)
-	}
 }
